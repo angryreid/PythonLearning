5835d5030ca36081144d03c8575278b6a94d06e7
angryreid <onyourwill@gmail.com>
2024-10-29 21:43:33 +0800

    Doc | update

---
+++ 正则表达式/test.py
@@ -0,0 +1,18 @@
+"""
+test - PythonLearning
+Author: nick
+Date: 2024/10/29
+Time: 21:38
+
+Description: 
+
+"""
+
+
+def main():
+    print(123)
+    pass  # TODO: Implement your code here
+
+
+if __name__ == "__main__":
+    main()
