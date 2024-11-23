586b3c89dfcacd442b4121e1e24657485e0a6c3b
angryreid <onyourwill@gmail.com>
2024-11-23 20:35:03 +0800

    module

---
+++ module/v2/import_module.py
@@ -0,0 +1,18 @@
+"""
+import_module - PythonLearning
+Author: nick
+Date: 2024/11/23
+Time: 20:32
+
+Description: 
+
+"""
+from math import log2
+
+
+def main():
+    print(f'log2(8): {log2(8)}')
+
+
+if __name__ == "__main__":
+    main()
