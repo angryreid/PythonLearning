1ae38fbaf130127bb550e989a31576ab6c4e535a
angryreid <onyourwill@gmail.com>
2024-12-27 22:39:01 +0800

    Regular expression

--- 正则表达式/v2/test.py
+++ 正则表达式/v2/test.py
@@ -7,11 +7,14 @@ Time: 21:38
 Description: 
 
 """
+import re
 
 
 def main():
-    print(123)
-    pass  # TODO: Implement your code here
+    s = 'hello world, today is a new day'
+    pattern = 'world'
+    res = re.match(pattern, s)
+    print(res)
 
 
 if __name__ == "__main__":
